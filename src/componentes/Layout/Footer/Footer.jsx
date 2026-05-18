9a5a00be65da87805375d8aae9a285f889902484
efileonardo-creator <efi.leonardo@gmail.com>
2026-05-18 18:03:14 -0300

    Creacion de archivo json

---
+++ src/componentes/Layout/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.module.css';
+
+function Footer() {
+return (
+    <footer>
+        <p>&copy; 2025 - Mi Aplicación React</p>
+    </footer>
+);
+}
+export default Footer;
---
+++ src/componentes/Layout/Header/Header.jsx
@@ -0,0 +1,9 @@
+import styles from'./Header.module.css'
+function Header() {
+return (
+    <header className={styles.header}>
+       <div>Bienvenidos a mi App React</div>
+    </header>
+);
+}
+export default Header;
